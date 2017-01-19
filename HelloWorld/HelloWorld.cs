acefa200f643703415d1e41f5cbb18c2663c60d8
brian <brian@BYTESTUFFSP4>
2017-01-19 15:44:26 -0500

    Add project files.

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+            Console.WriteLine("Hello World.");
+
+            Environment.Exit(200);
+        }
+    }
+}
